b85ba0f1e01f5102c4195f3a8bc058d975622fca
Caleb Ostrander <costrander@mail.greenriver.edu>
2017-05-17 19:02:06 -0500

    Added all the pages that will be needed later on

---
+++ classes/blogger.php
@@ -0,0 +1,157 @@
+<?php
+    /**
+     * This is a class that allows you to create a blogger for the blog site
+     *
+     * @author Caleb Ostrander
+     * @version 1.0
+     */
+    class Blogger
+    {
+        private $_username;
+        private $_email;
+        private $_password;
+        private $_photo;
+        private $_bio;
+        private $_id;
+        
+        /**
+         * A constructor for the Blogger class setting all the basic information
+         * for a blogger
+         *
+         * @param Usernmane
+         * @param Email
+         * @param Password
+         * @param Photo 
+         * @param Biography
+         * @param id
+         */
+        function __construct($username = "NA", $email = "NA", $password = "NA", $photo = "NA", $bio = "NA", $id = -1)
+        {
+            $this->_username = $username;
+            $this->_email = $email;
+            $this->_password = $password;
+            $this->_photo = $photo;
+            $this->_bio = $bio;
+            $this->_id = $id;
+        }
+        
+        /**
+         * A setter for their username
+         *
+         *@param Their username
+         */
+        function setUsername($username)
+        {
+            $this->_username = $username;
+        }
+        
+        /**
+         * A getter for their username
+         *
+         * @return Their username
+         */
+        function getUsername()
+        {
+            return $this->_username;
+        }
+        
+        /**
+         * A setter for their password
+         *
+         *@param Their password
+         */
+        function setPassword($password)
+        {
+            $this->_password = $password;
+        }
+        
+        /**
+         * A getter for their password
+         *
+         * @return Their password
+         */
+        function getPasswords()
+        {
+            return $this->_password;
+        }
+        
+        /**
+         * A setter for their email
+         *
+         *@param Their email
+         */
+        function setEmail($email)
+        {
+            $this->_email = $email;
+        }
+        
+        /**
+         * A getter for their email
+         *
+         * @return Their email
+         */
+        function getEmail()
+        {
+            return $this->_email;
+        }
+        
+        /**
+         * A setter for their photo
+         *
+         *@param Their photo
+         */
+        function setPhoto($photo)
+        {
+            $this->_photo = $photo;
+        }
+        
+        /**
+         * A getter for their photo
+         *
+         * @return Their photo
+         */
+        function getPhoto()
+        {
+            return $this->_photo;
+        }
+        
+        /**
+         * A setter for their ID
+         *
+         *@param Their ID
+         */
+        function setId($id)
+        {
+            $this->_id = $id;
+        }
+        
+        /**
+         * A getter for their id
+         *
+         * @return Their ID
+         */
+        function getId()
+        {
+            return $this->_id;
+        }
+        
+        /**
+         * A setter for their bio
+         *
+         *@param Their bio
+         */
+        function setBio($bio)
+        {
+            $this->_bio = $bio;
+        }
+        
+        /**
+         * A getter for their bio
+         *
+         * @return Their bio
+         */
+        function getBio()
+        {
+            return $this->_bio;
+        }
+    }
--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 /*
  * Caleb Ostrander
- * Dating site, IT 328
+ * Blog site, IT 328
  */
 
     require_once('vendor/autoload.php');
 	
 	session_start();
-	
-	$memberDB = new MemberDB();
 	
     $f3 = Base::instance();
 	
@@ -16,102 +14,8 @@
 	
     $f3->route('GET /', function()
     {
-        $view = new View;
-        echo $view->render
-            ('pages/home.html');
+        echo Template::instance()->render('pages/home.html');
     }
-    );
-    
-    $f3->route('GET /personal-information', function()
-        {
-            $view = new View;
-            echo $view->render('pages/personal-information.html');
-        }
-    );
-    
-    $f3->route('POST /profile', function()
-        {
-			if ($_POST['premium'])
-			{
-				$profile = new PremiumMember($_POST['firstName'], $_POST['lastName'], $_POST['age'], $_POST['gender'], $_POST['phone']);
-			} else
-			{
-				$profile = new Member($_POST['firstName'], $_POST['lastName'], $_POST['age'], $_POST['gender'], $_POST['phone']);
-			}
-			
-			$_SESSION['profile'] = $profile;
-			
-            $view = new View;
-            echo $view->render('pages/profile.html');
-        }
-    );
-    
-    $f3->route('POST /interests', function()
-        {
-			$profile = $_SESSION['profile'];
-			
-			$profile->setEmail($_POST['email']);
-			$profile->setState($_POST['state']);
-			$profile->setPref($_POST['pref']);
-			$profile->setBio($_POST['bio']);
-			
-			$_SESSION['profile'] = $profile;
-			
-            $view = new View;
-			
-			if (get_class($profile) == "PremiumMember") echo $view->render('pages/interests.html');
-			else $GLOBALS['f3']->reroute('/summary');
-        }
-    );
-    
-    $f3->route('GET|POST /summary', function()
-        {
-			$profile = $_SESSION['profile'];
-			$members = $GLOBALS['memberDB'];
-			
-			if ($profile->checkPremium())
-			{
-				$profile->setInDoorInterests($_POST['indoor']);
-				$profile->setOutDoorInterests($_POST['outdoor']);
-				
-				// Set upload if it is uploaded or unset it
-				if (move_uploaded_file($_FILES["photo"]["tmp_name"], "images/" . basename($_FILES["photo"]["name"])))
-				{
-					$profile->setPhoto($_FILES["photo"]["name"]);
-					$_SESSION['uploaded'] = true;
-				}
-				
-				$entryID = $members->addMember($profile);
-				$members->addPremiumMember($entryID, $profile);
-				$profile->setID($entryID);
-				
-			} else $profile->setID($members->addMember($profile));
-			
-			$_SESSION['profile'] = $profile;
-            
-            $view = new View;
-            echo $view->render('pages/summary.php');
-        }
-    );
-	
-	$f3->route('GET /admin', function($f3)
-    {
-		$members = $GLOBALS['memberDB']->allMembers();
-		$f3->set('members', $members);
-		
-        $view = new View;
-        echo $view->render
-            ('pages/admin.html');
-    }
-    );
-	
-	$f3->route('GET /@id', function($f3, $params)
-        {
-			$_SESSION['id'] = $params['id'];
-			$view = new View;
-			echo $view->render
-				('pages/premium.php');
-        }
     );
     
     $f3->run();
